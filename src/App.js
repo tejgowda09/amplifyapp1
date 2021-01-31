9f2572677899d6eb4fae8cb02bd707f892707227
thejas <thejastanzy@gmail.com>
2021-01-31 22:51:17 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from thejas</h1>
+        <h1>Hello from thejas we are now auth</h1>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          
+
         </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+
+export default withAuthenticator(App);
